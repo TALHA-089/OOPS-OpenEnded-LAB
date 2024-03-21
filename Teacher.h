b98bfdae146a43a19c6b10452e4467c82cae1450
Abbas9x <146324747+Abbas9x@users.noreply.github.com>
2024-03-21 13:29:14 +0500

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -1,15 +1,11 @@
 #pragma once
 #include<iostream>
 #include<vector>
+#include "Course.h"
 #include<string>
 
 using namespace std;
 
-
-
-
-
-class Course;
 
 class Teacher {
 
@@ -66,6 +62,35 @@ public:
 	void AssignCourse( const Course& course){
 
 		coursesTaught.push_back(course);
+
+	}
+
+	void removeCourse(Course& course) {
+		int count = 0;
+		for (auto it: coursesTaught) {
+			if (it.getCourseCode() == course.getCourseCode() ){
+				auto it1 = find(coursesTaught.begin(), coursesTaught.end(), count);
+			    coursesTaught.erase(it1);
+				cout << "\n\nCourse Removed!" << endl;
+				break;
+			}
+
+			count++;
+		}
+	}
+
+	void viewCourses() const {
+
+		int count = 0;
+		for (auto it : coursesTaught) {
+
+			cout << "\n\n-------------Course " << count + 1 << "-------------- ";
+			cout << "\n\nCourse Code: " << it.getCourseCode();
+			cout << "\n\nCourse Name: " << it.getCourseName();
+
+
+		}
+		
 
 	}
 
